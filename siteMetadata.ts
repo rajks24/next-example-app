ac494fcd4a4021a33753693633c69c2f7c5ae015
Rajesh Singh <rajeshsingh624@gmail.com>
2023-07-30 21:56:09 +1000

    updated meta

--- siteMetadata.ts
+++ siteMetadata.ts
@@ -6,15 +6,15 @@ const siteMetadata = {
   authorImage: "/images/image.jpg",
 
   email: "connect@txconsole.com",
-  github: "https://github.com/rajks24",
+  github: "https://github.com/rajks24/next-example-app.git",
   twitter: "https://twitter.com/rajinh24",
-  youtube: "https://www.youtube.com/@txconsole6529",
+  youtube: "https://www.youtube.com/@txconsole",
   linkedin: "https://linkedin.com/in/rajinh",
 
   siteUrl: "http://localhost:3000",
-  siteRepo: "https://github.com/rajks24",
+  siteRepo: "https://github.com/rajks24/next-example-app.git",
   siteLogo: "/images/nextapp.png",
-  
+
 
 };
 
